9d7afc6105907a8d35bd24c3e0431e88552aecf3
Wimsteels <wim.steels@gmail.com>
2023-03-22 20:43:16 +0100

    versie Wim

--- app.py
+++ app.py
@@ -17,12 +17,11 @@ def chooseRecipe():
     data = get_data.getData("reciperevolutiondb","recepten")
     return render_template("chooseRecipe.html",data = data)
 
-<<<<<<< HEAD
 @app.route("/materialen")
 def materialen():
     data = get_data.getData("reciperevolutiondb","materialen")
     return render_template("materialen.html",data = data)
-=======
+
 @app.route("/chooseIngredient")
 def chooseIngredient():
     data = get_data.getData("reciperevolutiondb","ingrediënten")
@@ -32,7 +31,6 @@ def chooseIngredient():
 def chooseRecipeIngredient():
     data = get_data.getData("reciperevolutiondb","recepten_ingrediënten")
     return render_template("chooseRecipeIngredient.html",data = data)
->>>>>>> 510f9e6d7bf331ff48c6f121eba2e65799164dc4
 
 if __name__ == '__main__':
     app.run()
